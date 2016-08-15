493e3f5c1b3ebf46199f2fc483afb73c8b9df34f
steve simpson <stevesimpson4375@gmail.com>
2016-08-14 21:43:22 -0400

    Made the button talk

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -26,5 +26,14 @@ namespace HelloWorld
         {
             this.InitializeComponent();
         }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
     }
 }
